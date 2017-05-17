5cc01a10a19a8354ad44dd242eed996c6eef7f37
Padraic O'Neill <pconeill@gmail.com [core]>
2017-05-17 08:22:01 -0600

    fix mispelling in user_view_all_artists_spec

---
+++ spec/features/user_views_all_artists_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature "User views all artists" do
+
+  scenario "they go to the artists index" do
+    artist = Artist.new(name: "Jerry Garcia", image_path: "http://cps-static.rovicorp.com/3/JPG_400/MI0003/146/MI0003146038.jpg")
+    artist_name = artist.name
+    artist.save
+
+    artist2 = Artist.new(name: "Neil Young", image_path: "http://captaingino.com/wp-content/uploads/2015/11/Neil-Young.jpg")
+    artist2_name = artist2.name
+    artist2.save
+
+    visit '/artists'
+
+    expect(page).to have_content artist_name
+    expect(page).to have_content artist2_name
+  end
+end
